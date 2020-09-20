116ae32c729e23efb15082345617ed98d2112b6b
LuhBezerra <lucileneifal2018@gmail.com>
2020-09-20 14:27:48 -0300

    added mobile landing

---
+++ src/pages/Landing/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+import searchIcon from '../../assets/images/icons/searchIcon.svg';
+
+import './styles.css';
+
+export default function Header() {
+  return (
+    <div id="page-landing" className="container">
+      <div id="page-landing-content">
+        <header className="header">
+          <div className="top-bar-container">
+            <img src={logoImg} alt="NutriMe"/>
+
+            <div className="top-bar-options">
+              <a href="">Home</a>
+              <a href="">Sobre</a>
+            </div>
+          </div>
+        </header>
+
+        <div id="page-landing-container" >
+          <div className="landing-container">
+            <h1>
+              Encontre todas <strong>informações 
+              nutricionais</strong> do seu alimento
+              preferido
+            </h1>
+
+            <div className="search-container">
+              <div className="select-block">
+                <select value="" id="">
+                  <option value="" disabled hidden>Escolha a categoria do alimento</option>
+                  <option value="" >teste</option>
+                </select>
+              </div>
+
+              <div className="button-container">
+                <a href="">Buscar</a>
+              </div>
+            </div>  
+
+            <div className="landing-image">
+              <img src={landingImg} alt="hero-image" className="hero-image"/>
+            </div>
+          </div>
+        </div>
+      </div>
+   </div>
+  )
+}
