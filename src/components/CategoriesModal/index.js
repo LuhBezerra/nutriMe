5577437ddcd213beb6ca11322ced0ebbd969a561
LuhBezerra <lucileneifal2018@gmail.com>
2020-09-22 14:30:40 -0300

    configured CategoriesModal

--- src/components/CategoriesModal/index.js
+++ src/components/CategoriesModal/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import closeImg from '../../assets/images/icons/close.svg';
+
 import './styles.css'
 
-const Modal = ({id="modal", onClose = () => {}, children}) => {
+const Modal = ({id="modal", onClose = () => {}, title}) => {
   const handleOutsideClick = (e) =>{
     if(e.target.id === id) onClose()
   }
@@ -10,8 +12,60 @@ const Modal = ({id="modal", onClose = () => {}, children}) => {
   return (
      <div id={id} className="modal" onClick={handleOutsideClick}>
        <div className="container">
-         <button className="close" onClick={onClose}/>
-         <div className="content">{children}</div>
+         <button className="close" onClick={onClose}>
+           <img src={closeImg} alt="botão-para-fechar"/>
+         </button>
+         <div className="content">
+           <h1>Nome do Item</h1>
+
+          <div className="info-container">
+            <div className="info-container-energy">
+              <h2>Valor <br/>energético</h2>
+              <p>123.5</p>
+              <p>kcal</p>
+            </div>
+
+            <div className="info-nutritional-container">
+              <div className="info-nutritinal-item-container">
+                <div className="info-nutritinal-item-title">
+                  Carboidratos
+                </div>
+                <div className="info-nutritinal-item-value">
+                  23.5 g
+                </div>
+              </div>
+
+              <div className="info-nutritinal-item-container">
+                <div className="info-nutritinal-item-title">
+                  Carboidratos
+                </div>
+                <div className="info-nutritinal-item-value">
+                  23.5 g
+                </div>
+              </div>
+
+              <div className="info-nutritinal-item-container">
+                <div className="info-nutritinal-item-title">
+                  Carboidratos
+                </div>
+                <div className="info-nutritinal-item-value">
+                  23.5 g
+                </div>
+              </div>
+
+              <div className="info-nutritinal-item-container">
+                <div className="info-nutritinal-item-title">
+                  Carboidratos
+                </div>
+                <div className="info-nutritinal-item-value">
+                  23.5 g
+                </div>
+              </div>
+
+            </div> 
+            
+           </div>
+         </div>
        </div>
      </div>
   )
