20c814669475e2b57708983712ab5f8833e4c872
LuhBezerra <lucileneifal2018@gmail.com>
2020-09-22 11:09:33 -0300

    added desktop landing page and fixed bugs

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,51 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
-import searchIcon from '../../assets/images/icons/searchIcon.svg';
+import Header from '../../components/Header';
 
 import './styles.css';
 
-export default function Header() {
+export default function LandingPage() {
   return (
     <div id="page-landing" >
-      <div id="page-landing-content">
-        <header className="header">
-          <div className="top-bar-container">
-            <img src={logoImg} alt="NutriMe"/>
-
-            <div className="top-bar-options">
-              <a href="">Home</a>
-              <a href="">Sobre</a>
-            </div>
-          </div>
-        </header>
+        <Header
+          option1="Home"
+          option2="Sobre"
+        />
 
         <div id="landing-container" className="container">
+          <div className="search-container">
             <h1>
-              Encontre todas <strong>informações 
-              nutricionais</strong> do seu alimento
+              Encontre todas <strong>informações<br/> 
+              nutricionais</strong> do seu alimento<br/>
               preferido
             </h1>
 
-            <div className="search-container">
-              <div className="select-block">
-                <select value="" id="">
-                  <option value="" disabled hidden>Escolha a categoria</option>
-                  <option value="" >teste</option>
-                </select>
-              </div>
+            <div className="select-block">
+              <select value="" id="">
+                <option value="" disabled hidden>Escolha a categoria</option>
+                <option value="" >teste</option>
+              </select>
 
               <div className="button-container">
-                <a href="">Buscar</a>
+                <Link to="/Categories">Buscar</Link>
               </div>
-            </div>  
-
+            </div>
+          </div>  
+          
             <div className="landing-image">
-              <img src={landingImg} alt="hero-image" className="hero-image"/>
+              <img src={landingImg} title="Vector criado por pch.vector - www.freepik.com" alt="hero" className="hero-image"/>
             </div>
         </div>
-      </div>
    </div>
   )
 }
